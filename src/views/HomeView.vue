8af6aef4035624931ad4825f16ac3c8011d9425a
Sid <siddharth.rajagopalan.btech2020@sitpune.edu.in>
2023-03-17 14:36:05 +0530

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Welcome to Librarian</h1>
+</template>
+
+<script>
+  import HelloWorld from '../components/HelloWorld'
+
+  export default {
+    name: 'Home'
+
+    
+  }
+  
+    
+
+  
+</script>
+
+<style scoped>
+      .test{
+        color: var(--primary);
+
+      }
+      </style>
